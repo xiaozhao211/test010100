24dabe307ae18e0916971eb01c9450b20af64bbf
xiaozhao <xiaozhao@haha.com>
2020-08-28 10:54:16 +0800

    333

--- src/com/xiaozhoa/domain/Person.java
+++ src/com/xiaozhoa/domain/Person.java
@@ -4,6 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Person {
     private String name;
+    private String address;
     public static void main(String[] args) {
         System.out.println("hh");
         System.out.println("nihao");
