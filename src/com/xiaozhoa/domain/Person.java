010e0737dd1ed20ff255c420dce6641e39396463
xiaozhao <xiaozhao@haha.com>
2020-08-28 10:47:41 +0800

    222

--- src/com/xiaozhoa/domain/Person.java
+++ src/com/xiaozhoa/domain/Person.java
@@ -3,6 +3,7 @@ package com.xiaozhoa.domain;
 import org.w3c.dom.ls.LSOutput;
 
 public class Person {
+    private String name;
     public static void main(String[] args) {
         System.out.println("hh");
         System.out.println("nihao");
